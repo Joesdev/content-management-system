cb222685ed1286a3de2566b1bb7d260316512b3f
Joesdev <Joesilvadev@gmail.com>
2017-02-20 13:14:56 -0800

    Added Javasript Chart, without data

--- admin/index.php
+++ admin/index.php
@@ -146,8 +146,33 @@
 							<!-- /.row -->
 							
 							
-							
-							
+			<div class="row">
+
+				<script type="text/javascript">
+				  google.charts.load('current', {'packages':['bar']});
+				  google.charts.setOnLoadCallback(drawChart);
+				  function drawChart() {
+					var data = google.visualization.arrayToDataTable([
+					  ['Data', 'Count'],
+					  ['Post', 1000],
+					]);
+
+					var options = {
+					  chart: {
+						title: '',
+						subtitle: '',
+					  }
+					};
+
+					var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+					chart.draw(data, options);
+				  }
+				</script>
+
+				<div id="columnchart_material" style="width: 'auto'; height: 500px;"></div>
+
+			</div>		
 							
 		</div>
 		<!-- /.container-fluid -->
