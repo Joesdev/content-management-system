364f0b18317e7454090bead0f591ca09de692356
Joesdev <Joesilvadev@gmail.com>
2017-02-23 20:08:08 -0800

    Added Select All Javascript Functionality

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -1,2 +1,15 @@
 // Post WYSIWYG Editor (what you see if what you get)
 tinymce.init({ selector:'textarea' });
+$(document).ready(function(){
+	$('#selectAllBoxes').click(function(event){
+		if(this.checked){
+			$('.checkBoxes').each(function(){
+				this.checked = true;
+			});
+		} else{
+			$('.checkBoxes').each(function(){
+				this.checked = false;
+			});
+		}
+	});
+});
