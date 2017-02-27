61c1d272f7050fd5df8863bbfabadbfd603e09b4
Joesdev <Joesilvadev@gmail.com>
2017-02-26 17:10:27 -0800

    Cleaned User Input, Added Crypt Blowfish

--- registration.php
+++ registration.php
@@ -1,5 +1,27 @@
 <?php  include "includes/db.php"; ?>
- <?php  include "includes/header.php"; ?>
+<?php  include "includes/header.php"; ?>
+<?php 
+	if(isset($_POST['submit'])){
+		
+		$username = $_POST['username'];
+		$email    = $_POST['email'];
+		$password = $_POST['password'];
+		
+		// Clean up input for security
+		echo $username = mysqli_real_escape_string($connection, $username);
+		echo $email    = mysqli_real_escape_string($connection, $email);
+		echo $password = mysqli_real_escape_string($connection, $password);
+		
+		$query = "SELECT user_randSalt FROM users" ;
+		$select_randsalt_query = mysqli_query($connection, $query);
+		if(!$select_randsalt_query){
+			die('QUERY FAILED!' . mysqli_error($connection));
+		}
+		
+	}
+
+?> 
+ 
 
 
     <!-- Navigation -->
