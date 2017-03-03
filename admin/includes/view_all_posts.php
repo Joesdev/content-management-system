875ccd392d778f26b70d09fac03f7d9a7347e015
Joesdev <Joesilvadev@gmail.com>
2017-03-02 19:54:16 -0800

    View Count Added To view_all_posts

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -93,6 +93,7 @@
 				<th>Dates</th>
 				<th>Edit</th>
 				<th>Delete</th>
+				<th>Views</th>
 			</tr>
 		</thead>
 
@@ -112,6 +113,7 @@
 				$post_Tags = $row['post_tags'];
 				$post_Comment_Count = $row['post_comment_count'];
 				$post_Date = $row['post_date'];
+				$post_view_count = $row['post_view_count'];
 
 				echo "<tr>";
 				?>
@@ -138,6 +140,7 @@
 				echo "<td> $post_Date </td>";
 				echo "<td><a href='posts.php?source=edit_post&p_id={$post_Id}'>Edit</a></td>";
 				echo "<td><a onClick=\"javascript: return confirm('Are You Sure You Want To Delete?'); \"href='posts.php?delete={$post_Id}'>Delete</a></td>";
+				echo "<td>{$post_view_count}</td>";
 				echo "</tr>";
 			}
 
