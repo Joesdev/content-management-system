b6105abafbe3a264fb99a28f54d21e295b9a4d94
Joesdev <Joesilvadev@gmail.com>
2017-03-02 19:32:21 -0800

    Set Up JQuery Loader, Not Implimented

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -15,4 +15,14 @@ $(document).ready(function(){
 			});
 		}
 	});
+	
+/*	//Jquery
+	var div_box ="<div id='load-screen'><div id='loading'></div></div>";
+	
+	$("body").prepend(div_box);
+	$('#load-screen').delay(700).fadeOut(600, function(){
+		$(this).remove();
+	});*/
+	
+	
 });
