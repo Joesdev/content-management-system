66f5ac999d76fb2c65861355611e7f2057d7559b
Joesdev <Joesilvadev@gmail.com>
2017-03-07 18:24:59 -0800

    function loadUsersOnline Finished, Not Tested

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -26,3 +26,19 @@ $(document).ready(function(){
 	
 	
 });
+
+function loadUsersOnline(){
+	$.get("functions.php?onlineusers=result", function(data){
+		$(".usersonline").text(data);
+	});
+}
+
+
+loadUsersOnline();
+
+
+
+
+
+
+
