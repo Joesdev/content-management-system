f021e305878f42ccbf495f5d1f84bbe7905006f6
Joesdev <Joesilvadev@gmail.com>
2017-04-01 14:39:04 -0700

    Modified input fields of input and textbody

--- contact.php
+++ contact.php
@@ -51,20 +51,19 @@
         <div class="row">
             <div class="col-xs-6 col-xs-offset-3">
                 <div class="form-wrap">
-                <h1>Contact Me</h1>
+                <h1>Contact Page</h1>
                     <form role="form" action="registration.php" method="post" id="login-form" autocomplete="off">
-                       <h6 class="text-center"><?php echo $message; ?></h6>
-                        <div class="form-group">
-                            <label for="username" class="sr-only">username</label>
-                            <input type="text" name="username" id="username" class="form-control" placeholder="Enter Desired Username">
-                        </div>
                          <div class="form-group">
                             <label for="email" class="sr-only">Email</label>
-                            <input type="email" name="email" id="email" class="form-control" placeholder="somebody@example.com">
+                            <input type="email" name="email" id="email" class="form-control" placeholder="Email">
+                        </div>
+                        <div class="form-group">
+                            <label for="email" class="sr-only">Subject</label>
+                            <input type="subject" name="subject" id="subject" class="form-control" placeholder="What's The Subject?">
                         </div>
                          <div class="form-group">
-                            <label for="password" class="sr-only">Password</label>
-                            <input type="password" name="password" id="key" class="form-control" placeholder="Password">
+                            <textarea class="form-control" name="body" id="body"></textarea>
+                            
                         </div>
                 
                         <input type="submit" name="submit" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Register">
